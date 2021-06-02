cc2dc1e7faebe3237f40953a6f86686b03745e87
Andy Freeland <andy@andyfreeland.net>
2021-06-02 00:18:31 -0700

    [8.4.2] Refactor `Add::run()`
    This patch breaks out the error handling and add-to-index logic in
    `Add::run()` to separate functions.

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -2,6 +2,7 @@ use crate::commands::CommandContext;
 use crate::database::blob::Blob;
 use crate::database::object::Object;
 use crate::errors::{Error, Result};
+use crate::repository::Repository;
 use std::io;
 use std::io::Read;
 use std::path::PathBuf;
@@ -17,71 +18,79 @@ impl Add {
 
         match ctx.repo.index.load_for_update() {
             Ok(()) => (),
-            Err(err) => match err {
-                Error::LockDenied(..) => {
-                    eprintln!("fatal: {}", err);
-                    eprintln!(
-                        "
-Another jit process seems to be running in this repository.
-Please make sure all processes are terminated then try again.
-If it still fails, a jit process may have crashed in this
-repository earlier: remove the file manually to continue."
-                    );
-                    return Err(Error::Exit(128));
-                }
-                _ => return Err(err),
-            },
+            Err(err) => return Self::handle_locked_index(err),
         }
 
         for path in ctx.argv.range(0..) {
             let path = match PathBuf::from(path).canonicalize() {
                 Ok(path) => path,
-                Err(err) => {
-                    if err.kind() == io::ErrorKind::NotFound {
-                        eprintln!("fatal: pathspec '{}' did not match any files", path);
-                        ctx.repo.index.release_lock()?;
-                        return Err(Error::Exit(128));
-                    } else {
-                        return Err(Error::Io(err));
-                    }
-                }
+                Err(err) => return Self::handle_missing_file(ctx.repo, path, err),
             };
 
             for path in ctx.repo.workspace.list_files(&path)? {
-                let data = match ctx.repo.workspace.read_file(&path) {
-                    Ok(data) => data,
-                    Err(err) => match err {
-                        Error::NoPermission { .. } => {
-                            eprintln!("error: {}", err);
-                            eprintln!("fatal: adding files failed");
-                            ctx.repo.index.release_lock()?;
-                            return Err(Error::Exit(128));
-                        }
-                        _ => return Err(err),
-                    },
-                };
-                let stat = match ctx.repo.workspace.stat_file(&path) {
-                    Ok(stat) => stat,
-                    Err(err) => match err {
-                        Error::NoPermission { .. } => {
-                            eprintln!("error: {}", err);
-                            eprintln!("fatal: adding files failed");
-                            ctx.repo.index.release_lock()?;
-                            return Err(Error::Exit(128));
-                        }
-                        _ => return Err(err),
-                    },
-                };
-
-                let blob = Blob::new(data);
-                ctx.repo.database.store(&blob)?;
-                ctx.repo.index.add(path, blob.oid(), stat);
+                Self::add_to_index(&mut ctx.repo, path)?;
             }
         }
 
         ctx.repo.index.write_updates()?;
 
         Ok(())
+    }
+
+    fn add_to_index(repo: &mut Repository, path: PathBuf) -> Result<()> {
+        let data = match repo.workspace.read_file(&path) {
+            Ok(data) => data,
+            Err(err) => return Self::handle_unreadable_file(repo, err),
+        };
+        let stat = match repo.workspace.stat_file(&path) {
+            Ok(stat) => stat,
+            Err(err) => return Self::handle_unreadable_file(repo, err),
+        };
+
+        let blob = Blob::new(data);
+        repo.database.store(&blob)?;
+        repo.index.add(path, blob.oid(), stat);
+
+        Ok(())
+    }
+
+    fn handle_locked_index(err: Error) -> Result<()> {
+        match err {
+            Error::LockDenied(..) => {
+                eprintln!("fatal: {}", err);
+                eprintln!(
+                    "
+Another jit process seems to be running in this repository.
+Please make sure all processes are terminated then try again.
+If it still fails, a jit process may have crashed in this
+repository earlier: remove the file manually to continue."
+                );
+                Err(Error::Exit(128))
+            }
+            _ => Err(err),
+        }
+    }
+
+    fn handle_missing_file(mut repo: Repository, path: &str, err: io::Error) -> Result<()> {
+        if err.kind() == io::ErrorKind::NotFound {
+            eprintln!("fatal: pathspec '{}' did not match any files", path);
+            repo.index.release_lock()?;
+            Err(Error::Exit(128))
+        } else {
+            Err(Error::Io(err))
+        }
+    }
+
+    fn handle_unreadable_file(repo: &mut Repository, err: Error) -> Result<()> {
+        match err {
+            Error::NoPermission { .. } => {
+                eprintln!("error: {}", err);
+                eprintln!("fatal: adding files failed");
+                repo.index.release_lock()?;
+                Err(Error::Exit(128))
+            }
+            _ => Err(err),
+        }
     }
 }
 

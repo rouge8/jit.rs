47066350ce04789c9a2e3344b606a571c07ee4b5
Andy Freeland <andy@andyfreeland.net>
2021-06-10 22:57:27 -0700

    [12.1] Extract status logic into `Repository`
    This sets us up to work on the `diff` command. In order to know which
    files we should calculate and display the diffs of, without reading
    every file in the tree, we need to know which files differ between the
    HEAD commit, the index, and the workspace. That is, we need exactly the
    information that the `status` command displays.
    
    Here we extract the business logic from `Status` into `Repository` so
    that it can be reused by other commands. The moved code is kept pretty
    much verbatim, and the only code left in `Status` is that related to
    printing the output, and opening and writing the index. In the book,
    they create a separate `Repository::Status` class that has a reference
    to its parent `Repository` and this quickly became a mess in Rust, so I
    put all of its methods on `Repository` instead.
    
    The index logic should remain in the top-level command code rather than
    moving into reused business logic, because it's a piece of global state.
    If the top-level command has opened the index for updates and a piece of
    business logic then attempts the same thing, the second call will fail.
    Therefore we leave top-level commands in charge of how the index is
    opened.

--- src/commands/status.rs
+++ src/commands/status.rs
@@ -1,40 +1,13 @@
 use crate::commands::CommandContext;
-use crate::database::blob::Blob;
-use crate::database::tree::TreeEntry;
-use crate::database::ParsedObject;
 use crate::errors::Result;
-use crate::index::Entry;
-use crate::repository::Repository;
-use crate::util::path_to_string;
+use crate::repository::{ChangeType, Repository};
 use colored::Colorize;
 use lazy_static::lazy_static;
-use std::collections::{BTreeMap, BTreeSet, HashMap, VecDeque};
-use std::fs;
-use std::path::{Path, PathBuf, MAIN_SEPARATOR};
+use std::collections::{BTreeMap, HashMap, VecDeque};
 
 pub struct Status {
-    root_dir: PathBuf,
     repo: Repository,
     argv: VecDeque<String>,
-    stats: HashMap<String, fs::Metadata>,
-    changed: BTreeSet<String>,
-    index_changes: BTreeMap<String, ChangeType>,
-    workspace_changes: BTreeMap<String, ChangeType>,
-    untracked: BTreeSet<String>,
-    head_tree: HashMap<String, TreeEntry>,
-}
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-enum ChangeType {
-    Added,
-    Deleted,
-    Modified,
-}
-
-#[derive(Debug)]
-enum ChangeKind {
-    Workspace,
-    Index,
 }
 
 lazy_static! {
@@ -59,94 +32,19 @@ static LABEL_WIDTH: usize = 12;
 impl Status {
     pub fn new(ctx: CommandContext) -> Self {
         Self {
-            root_dir: ctx.dir,
             repo: ctx.repo,
             argv: ctx.argv,
-            stats: HashMap::new(),
-            changed: BTreeSet::new(),
-            index_changes: BTreeMap::new(),
-            workspace_changes: BTreeMap::new(),
-            untracked: BTreeSet::new(),
-            head_tree: HashMap::new(),
         }
     }
 
     pub fn run(&mut self) -> Result<()> {
         self.repo.index.load_for_update()?;
-
-        self.scan_workspace(&self.root_dir.clone())?;
-        self.load_head_tree()?;
-        self.check_index_entries()?;
-        self.collect_deleted_head_files();
-
+        self.repo.initialize_status()?;
         self.repo.index.write_updates()?;
 
         self.print_results();
 
         Ok(())
-    }
-
-    fn load_head_tree(&mut self) -> Result<()> {
-        let head_oid = self.repo.refs.read_head()?;
-
-        if let Some(head_oid) = head_oid {
-            let commit = match self.repo.database.load(head_oid)? {
-                ParsedObject::Commit(commit) => commit,
-                _ => unreachable!(),
-            };
-            let tree_oid = commit.tree.clone();
-            self.read_tree(tree_oid, PathBuf::new())?;
-        }
-
-        Ok(())
-    }
-
-    fn read_tree(&mut self, tree_oid: String, pathname: PathBuf) -> Result<()> {
-        let tree = match self.repo.database.load(tree_oid)? {
-            ParsedObject::Tree(tree) => tree,
-            _ => unreachable!(),
-        };
-
-        let entries = tree.entries.clone();
-        for (name, entry) in entries {
-            let path = pathname.join(name);
-
-            if entry.is_tree() {
-                self.read_tree(entry.oid(), path)?;
-            } else {
-                self.head_tree.insert(path_to_string(&path), entry);
-            }
-        }
-
-        Ok(())
-    }
-
-    fn check_index_entries(&mut self) -> Result<()> {
-        // We have to iterate over `cloned_entries` rather than `self.repo.index.entries` because
-        // Rust will not let us borrow self as mutable more than one time: first with
-        // `self.repo.index.entries.values_mut()` and second with `self.check_index_entry()`.
-        let mut cloned_entries = self.repo.index.entries.clone();
-        for mut entry in cloned_entries.values_mut() {
-            self.check_index_against_workspace(&mut entry)?;
-            self.check_index_against_head_tree(&entry);
-        }
-
-        // Update `self.repo.index.entries` with the entries that were modified in
-        // `self.check_index_entry()`
-        for (key, val) in cloned_entries {
-            self.repo.index.entries.insert(key, val);
-        }
-
-        Ok(())
-    }
-
-    fn collect_deleted_head_files(&mut self) {
-        let keys: Vec<_> = self.head_tree.keys().cloned().collect();
-        for path in keys {
-            if !self.repo.index.tracked_file(Path::new(&path)) {
-                self.record_change(&path, ChangeKind::Index, ChangeType::Deleted);
-            }
-        }
     }
 
     fn print_results(&self) {
@@ -158,20 +56,20 @@ impl Status {
     }
 
     fn print_porcelain_format(&self) {
-        for path in &self.changed {
+        for path in &self.repo.changed {
             let status = self.status_for(&path);
             println!("{} {}", status, path);
         }
-        for path in &self.untracked {
+        for path in &self.repo.untracked_files {
             println!("?? {}", path);
         }
     }
 
     fn print_long_format(&self) {
-        self.print_changeset("Changes to be committed", &self.index_changes, "green");
+        self.print_changeset("Changes to be committed", &self.repo.index_changes, "green");
         self.print_changeset(
             "Changes not staged for commit",
-            &self.workspace_changes,
+            &self.repo.workspace_changes,
             "red",
         );
         self.print_untracked_files();
@@ -199,26 +97,26 @@ impl Status {
     }
 
     fn print_untracked_files(&self) {
-        if self.untracked.is_empty() {
+        if self.repo.untracked_files.is_empty() {
             return;
         }
 
         println!("Untracked files:");
         println!();
-        for path in &self.untracked {
+        for path in &self.repo.untracked_files {
             println!("{}", format!("\t{}", path).red());
         }
         println!();
     }
 
     fn print_commit_status(&self) {
-        if !self.index_changes.is_empty() {
+        if !self.repo.index_changes.is_empty() {
             return;
         }
 
-        if !self.workspace_changes.is_empty() {
+        if !self.repo.workspace_changes.is_empty() {
             println!("no changes added to commit");
-        } else if !self.untracked.is_empty() {
+        } else if !self.repo.untracked_files.is_empty() {
             println!("nothing added to commit but untracked files present");
         } else {
             println!("nothing to commit, working tree clean");
@@ -226,108 +124,15 @@ impl Status {
     }
 
     fn status_for(&self, path: &str) -> String {
-        let left = match self.index_changes.get(path) {
+        let left = match self.repo.index_changes.get(path) {
             Some(change) => SHORT_STATUS[change],
             None => " ",
         };
-        let right = match self.workspace_changes.get(path) {
+        let right = match self.repo.workspace_changes.get(path) {
             Some(change) => SHORT_STATUS[change],
             None => " ",
         };
 
         left.to_owned() + right
-    }
-
-    fn scan_workspace(&mut self, prefix: &Path) -> Result<()> {
-        for (path, stat) in &self.repo.workspace.list_dir(prefix)? {
-            if self.repo.index.tracked(path) {
-                if stat.is_file() {
-                    self.stats.insert(path_to_string(path), stat.clone());
-                } else if stat.is_dir() {
-                    self.scan_workspace(&path)?;
-                }
-            } else if self.trackable_file(&path, &stat)? {
-                let mut path = path_to_string(path);
-                if stat.is_dir() {
-                    path.push(MAIN_SEPARATOR);
-                }
-                self.untracked.insert(path);
-            }
-        }
-
-        Ok(())
-    }
-
-    fn record_change(&mut self, path: &str, change_kind: ChangeKind, r#type: ChangeType) {
-        self.changed.insert(path.to_string());
-
-        let changes = match change_kind {
-            ChangeKind::Index => &mut self.index_changes,
-            ChangeKind::Workspace => &mut self.workspace_changes,
-        };
-
-        changes.insert(path.to_string(), r#type);
-    }
-
-    fn trackable_file(&mut self, path: &Path, stat: &fs::Metadata) -> Result<bool> {
-        if stat.is_file() {
-            return Ok(!self.repo.index.tracked(path));
-        } else if !stat.is_dir() {
-            return Ok(false);
-        }
-
-        let items = self.repo.workspace.list_dir(path)?;
-        let files = items.iter().filter(|(_, item_stat)| item_stat.is_file());
-        let dirs = items.iter().filter(|(_, item_stat)| item_stat.is_dir());
-
-        for (item_path, item_stat) in files.chain(dirs) {
-            if self.trackable_file(item_path, item_stat)? {
-                return Ok(true);
-            }
-        }
-
-        Ok(false)
-    }
-
-    fn check_index_against_workspace(&mut self, entry: &mut Entry) -> Result<()> {
-        let stat = match self.stats.get(&entry.path) {
-            Some(stat) => stat,
-            None => {
-                self.record_change(&entry.path, ChangeKind::Workspace, ChangeType::Deleted);
-                return Ok(());
-            }
-        };
-
-        if !entry.stat_match(&stat) {
-            self.record_change(&entry.path, ChangeKind::Workspace, ChangeType::Modified);
-            return Ok(());
-        }
-
-        if entry.times_match(&stat) {
-            return Ok(());
-        }
-
-        let data = self.repo.workspace.read_file(&PathBuf::from(&entry.path))?;
-        let blob = Blob::new(data);
-        let oid = self.repo.database.hash_object(&blob);
-
-        if entry.oid == oid {
-            self.repo.index.update_entry_stat(entry, &stat);
-        } else {
-            self.record_change(&entry.path, ChangeKind::Workspace, ChangeType::Modified);
-        }
-
-        Ok(())
-    }
-
-    fn check_index_against_head_tree(&mut self, entry: &Entry) {
-        match self.head_tree.get(&entry.path) {
-            Some(item) => {
-                if entry.mode != item.mode() || entry.oid != item.oid() {
-                    self.record_change(&entry.path, ChangeKind::Index, ChangeType::Modified);
-                }
-            }
-            None => self.record_change(&entry.path, ChangeKind::Index, ChangeType::Added),
-        }
     }
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,26 +1,225 @@
-use crate::database::Database;
+use crate::database::{blob::Blob, tree::TreeEntry, Database, ParsedObject};
+use crate::errors::Result;
+use crate::index::Entry;
 use crate::index::Index;
 use crate::refs::Refs;
+use crate::util::path_to_string;
 use crate::workspace::Workspace;
-use std::path::PathBuf;
+use std::collections::{BTreeMap, BTreeSet, HashMap};
+use std::fs;
+use std::path::{Path, PathBuf, MAIN_SEPARATOR};
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum ChangeType {
+    Added,
+    Deleted,
+    Modified,
+}
+
+#[derive(Debug)]
+enum ChangeKind {
+    Workspace,
+    Index,
+}
 
 #[derive(Debug)]
 pub struct Repository {
-    git_path: PathBuf,
+    root_path: PathBuf,
     pub database: Database,
     pub index: Index,
     pub refs: Refs,
     pub workspace: Workspace,
+
+    // status-related fields
+    stats: HashMap<String, fs::Metadata>,
+    pub changed: BTreeSet<String>,
+    pub index_changes: BTreeMap<String, ChangeType>,
+    pub workspace_changes: BTreeMap<String, ChangeType>,
+    pub untracked_files: BTreeSet<String>,
+    head_tree: HashMap<String, TreeEntry>,
 }
 
 impl Repository {
     pub fn new(git_path: PathBuf) -> Self {
+        let root_path = git_path.parent().unwrap().to_path_buf();
+
         Repository {
-            git_path: git_path.clone(),
+            root_path,
             database: Database::new(git_path.join("objects")),
             index: Index::new(git_path.join("index")),
             refs: Refs::new(git_path.clone()),
             workspace: Workspace::new(git_path.parent().unwrap().to_path_buf()),
+            stats: HashMap::new(),
+            changed: BTreeSet::new(),
+            index_changes: BTreeMap::new(),
+            workspace_changes: BTreeMap::new(),
+            untracked_files: BTreeSet::new(),
+            head_tree: HashMap::new(),
+        }
+    }
+
+    pub fn initialize_status(&mut self) -> Result<()> {
+        self.scan_workspace(&self.root_path.clone())?;
+        self.load_head_tree()?;
+        self.check_index_entries()?;
+        self.collect_deleted_head_files();
+
+        Ok(())
+    }
+
+    fn record_change(&mut self, path: &str, change_kind: ChangeKind, r#type: ChangeType) {
+        self.changed.insert(path.to_string());
+
+        let changes = match change_kind {
+            ChangeKind::Index => &mut self.index_changes,
+            ChangeKind::Workspace => &mut self.workspace_changes,
+        };
+
+        changes.insert(path.to_string(), r#type);
+    }
+
+    fn scan_workspace(&mut self, prefix: &Path) -> Result<()> {
+        for (path, stat) in &self.workspace.list_dir(prefix)? {
+            if self.index.tracked(path) {
+                if stat.is_file() {
+                    self.stats.insert(path_to_string(path), stat.clone());
+                } else if stat.is_dir() {
+                    self.scan_workspace(&path)?;
+                }
+            } else if self.trackable_file(&path, &stat)? {
+                let mut path = path_to_string(path);
+                if stat.is_dir() {
+                    path.push(MAIN_SEPARATOR);
+                }
+                self.untracked_files.insert(path);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn trackable_file(&mut self, path: &Path, stat: &fs::Metadata) -> Result<bool> {
+        if stat.is_file() {
+            return Ok(!self.index.tracked(path));
+        } else if !stat.is_dir() {
+            return Ok(false);
+        }
+
+        let items = self.workspace.list_dir(path)?;
+        let files = items.iter().filter(|(_, item_stat)| item_stat.is_file());
+        let dirs = items.iter().filter(|(_, item_stat)| item_stat.is_dir());
+
+        for (item_path, item_stat) in files.chain(dirs) {
+            if self.trackable_file(item_path, item_stat)? {
+                return Ok(true);
+            }
+        }
+
+        Ok(false)
+    }
+
+    fn load_head_tree(&mut self) -> Result<()> {
+        let head_oid = self.refs.read_head()?;
+
+        if let Some(head_oid) = head_oid {
+            let commit = match self.database.load(head_oid)? {
+                ParsedObject::Commit(commit) => commit,
+                _ => unreachable!(),
+            };
+            let tree_oid = commit.tree.clone();
+            self.read_tree(tree_oid, PathBuf::new())?;
+        }
+
+        Ok(())
+    }
+
+    fn read_tree(&mut self, tree_oid: String, pathname: PathBuf) -> Result<()> {
+        let tree = match self.database.load(tree_oid)? {
+            ParsedObject::Tree(tree) => tree,
+            _ => unreachable!(),
+        };
+
+        let entries = tree.entries.clone();
+        for (name, entry) in entries {
+            let path = pathname.join(name);
+
+            if entry.is_tree() {
+                self.read_tree(entry.oid(), path)?;
+            } else {
+                self.head_tree.insert(path_to_string(&path), entry);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn check_index_entries(&mut self) -> Result<()> {
+        // We have to iterate over `cloned_entries` rather than `self.index.entries` because
+        // Rust will not let us borrow self as mutable more than one time: first with
+        // `self.index.entries.values_mut()` and second with `self.check_index_entry()`.
+        let mut cloned_entries = self.index.entries.clone();
+        for mut entry in cloned_entries.values_mut() {
+            self.check_index_against_workspace(&mut entry)?;
+            self.check_index_against_head_tree(&entry);
+        }
+
+        // Update `self.index.entries` with the entries that were modified in
+        // `self.check_index_entry()`
+        for (key, val) in cloned_entries {
+            self.index.entries.insert(key, val);
+        }
+
+        Ok(())
+    }
+
+    fn check_index_against_workspace(&mut self, entry: &mut Entry) -> Result<()> {
+        let stat = match self.stats.get(&entry.path) {
+            Some(stat) => stat,
+            None => {
+                self.record_change(&entry.path, ChangeKind::Workspace, ChangeType::Deleted);
+                return Ok(());
+            }
+        };
+
+        if !entry.stat_match(&stat) {
+            self.record_change(&entry.path, ChangeKind::Workspace, ChangeType::Modified);
+            return Ok(());
+        }
+
+        if entry.times_match(&stat) {
+            return Ok(());
+        }
+
+        let data = self.workspace.read_file(&PathBuf::from(&entry.path))?;
+        let blob = Blob::new(data);
+        let oid = self.database.hash_object(&blob);
+
+        if entry.oid == oid {
+            self.index.update_entry_stat(entry, &stat);
+        } else {
+            self.record_change(&entry.path, ChangeKind::Workspace, ChangeType::Modified);
+        }
+
+        Ok(())
+    }
+
+    fn check_index_against_head_tree(&mut self, entry: &Entry) {
+        match self.head_tree.get(&entry.path) {
+            Some(item) => {
+                if entry.mode != item.mode() || entry.oid != item.oid() {
+                    self.record_change(&entry.path, ChangeKind::Index, ChangeType::Modified);
+                }
+            }
+            None => self.record_change(&entry.path, ChangeKind::Index, ChangeType::Added),
+        }
+    }
+
+    fn collect_deleted_head_files(&mut self) {
+        let keys: Vec<_> = self.head_tree.keys().cloned().collect();
+        for path in keys {
+            if !self.index.tracked_file(Path::new(&path)) {
+                self.record_change(&path, ChangeKind::Index, ChangeType::Deleted);
+            }
         }
     }
 }

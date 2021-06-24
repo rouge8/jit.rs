3d38f97ecd7c1c7f5a17135f45cf21cd0356ebb6
Andy Freeland <andy@andyfreeland.net>
2021-06-24 11:06:45 -0700

    [15.4.1] Set the default branch to "main"
    Now that the `commit` command will update branch pointers rather than
    overwriting `.git/HEAD`, we alter `init` so that a new repository is set
    up to keep commits on a branch by default rather than just using HEAD
    until the user introduces a branch.
    
    Git's default branch is `master`, but we want to use `main`, and we can
    bring this about simply by creating the `.git/refs/heads` directory on
    init, and placing the following content in `.git/HEAD`:
    
        ref: refs/heads/main

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,7 +1,10 @@
 use crate::commands::CommandContext;
 use crate::errors::Result;
+use crate::refs::Refs;
 use std::fs;
 use std::io::Write;
+
+const DEFAULT_BRANCH: &str = "main";
 
 pub struct Init<E: Write> {
     ctx: CommandContext<E>,
@@ -21,9 +24,13 @@ impl<E: Write> Init<E> {
 
         let git_path = root_path.join(".git");
 
-        for dir in ["objects", "refs"].iter() {
+        for dir in ["objects", "refs/heads"].iter() {
             fs::create_dir_all(git_path.join(dir))?;
         }
+
+        let refs = Refs::new(git_path.clone());
+        let path = format!("refs/heads/{}", DEFAULT_BRANCH);
+        refs.update_head(format!("ref: {}", path))?;
 
         let mut stdout = self.ctx.stdout.borrow_mut();
         writeln!(stdout, "Initialized empty Jit repository in {:?}", git_path)?;
